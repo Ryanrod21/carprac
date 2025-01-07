b305530f178b6c31c8118ed7c4c0b136c4b61128
Ryan Rodriguez <ryancode2321@gmail.com>
2025-01-07 17:22:05 -0600

    next

---
+++ app.js
@@ -0,0 +1,9 @@
+// function toggleOverlay() {
+//   const overlay = document.getElementById('overlay');
+//   // Toggle the display of the overlay
+//   if (overlay.style.display === 'flex') {
+//     overlay.style.display = 'none'; // Hide the overlay
+//   } else {
+//     overlay.style.display = 'flex'; // Show the overlay
+//   }
+// }
